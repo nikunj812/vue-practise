801ef48d2dacf09aed33432adc51e9484f60fe19
vaghasiyaankur <ankurvaghasiya21@gmail.com>
2022-05-26 10:55:56 +0530

    add project

---
+++ src/App2.vue
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style>
+@import './assets/images/favicon.ico';
+@import './assets/style.css';
+@import './assets/bootstrap.min.css';
+@import './assets/helper.css';
+@import './assets/plugins.css';
+@import './assets/iconfont.min.css';
+</style>
